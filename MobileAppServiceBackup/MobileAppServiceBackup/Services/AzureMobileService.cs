003dfb45b68fd1dd1127df5928d488b831be36ab
Bryan Anthony Garcia <Bryan Anthony Garcia>
2017-04-20 02:02:24 +0800

    Add project files.

---
+++ MobileAppServiceBackup/MobileAppServiceBackup/Services/AzureMobileService.cs
@@ -0,0 +1,106 @@
+﻿using MobileAppServiceBackup.Models;
+using MobileAppServiceBackup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AzureMobileService))]
+namespace MobileAppServiceBackup.Services
+{
+    public class AzureMobileService
+    {
+        private string appUrl = "";
+        List<Debt> _debts;
+
+        public AzureMobileService()
+        {
+        }
+
+        private async Task Initialize()
+        {
+            await Task.Delay(2000);
+            if (_debts == null)
+            {
+                _debts = new List<Debt>()
+                {
+                    new Debt()
+                    {
+                        Id = 1,
+                        Name = "Bryan Anthony",
+                        Amount = 3000
+                    },
+                    new Debt()
+                    {
+                        Id = 2,
+                        Name = "Jacky Alfred",
+                        Amount = 2000
+                    },
+                    new Debt()
+                    {
+                        Id = 1,
+                        Name = "Angelo",
+                        Amount = 1500
+                    },
+                    new Debt()
+                    {
+                        Id = 1,
+                        Name = "Philip",
+                        Amount = 1000
+                    },
+                };
+            }
+        }
+
+        public async Task<List<Debt>> GetAllDebts()
+        {
+            await Initialize();
+            return _debts.Where(d => d.Amount > 0).ToList();
+        }
+
+        public async Task<bool> AddDebt(Debt debt)
+        {
+            try
+            {
+                await Initialize();
+                _debts.Add(debt);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateDebt(Debt debt)
+        {
+            try
+            {
+                await Initialize();
+                //_debts.Where(d => d.Id == debt.Id).FirstOrDefault();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> SyncDebts()
+        {
+            try
+            {
+                await Initialize();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+
+}
---
+++ MobileAppServiceBackup/MobileAppServiceBackup/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MobileAppServiceBackup.Models;
+using MobileAppServiceBackup.Services;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace MobileAppServiceBackup.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private AzureMobileService _azureMobileService;
+
+        private List<Debt> _debts;
+        public List<Debt> Debts
+        {
+            get { return _debts; }
+            set { SetProperty(ref _debts, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            _azureMobileService = DependencyService.Get<AzureMobileService>();
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Debts = await _azureMobileService.GetAllDebts();
+        }
+    }
+}
